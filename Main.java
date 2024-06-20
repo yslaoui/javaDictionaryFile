e387b05434119e7bfdeee03539b084bf4aeed072
yslaoui <yslaouiyassine@gmail.com>
2024-06-20 16:39:46 +0100

    Reading transalation data from file and loading it into a dictionary

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@ package dictionary;
 
 public class Main {
     public static void main(String[] args) {
-
+        SaveableDictionary saveableDictionary = new SaveableDictionary("src/main/java/dictionary/words.txt");
+        boolean wasSuccessful = saveableDictionary.load();
+        if (wasSuccessful) {
+            System.out.println("Successfully loaded the dictionary from file");
+        }
+        System.out.println(saveableDictionary.translate("apina"));
+        System.out.println(saveableDictionary.translate("ohjelmointi"));
+        System.out.println(saveableDictionary.translate("alla oleva"));
     }
 }
---
+++ SaveableDictionary.java
@@ -0,0 +1,72 @@
+package dictionary;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+public class SaveableDictionary {
+    Map<String, String> dictionary;
+    String fileName;
+
+    public SaveableDictionary() {
+        dictionary = new HashMap<>();
+    }
+
+    public SaveableDictionary(String fileName) {
+        this();
+        this.fileName = fileName;
+    }
+
+    public void add(String word, String translation) {
+        this.dictionary.putIfAbsent(word, translation);
+        this.dictionary.putIfAbsent(translation, word);
+    }
+
+    public String translate(String word) {
+        return this.dictionary.get(word);
+    }
+
+    public void delete(String word) {
+        this.dictionary.remove(this.dictionary.get(word));
+        this.dictionary.remove(word);
+    }
+
+//    public boolean load() {
+//        Path path = Paths.get(this.fileName);
+//        try (Stream<String> file = Files.lines(path)) {
+//            file
+//                    .map(row -> row.split(":"))
+//                    .forEach(rowArray->{
+//                        this.dictionary.putIfAbsent(rowArray[0], rowArray[1]);
+//                        this.dictionary.putIfAbsent(rowArray[1], rowArray[0]);
+//            });
+//            return true;
+//        } catch(IOException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+
+    public boolean load() {
+        File file = new File(this.fileName);
+        try (Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNext()) {
+                String row = scanner.nextLine();
+                String[] parts = row.split(":");
+                this.dictionary.putIfAbsent(parts[0], parts[1]);
+            }
+            return true;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
